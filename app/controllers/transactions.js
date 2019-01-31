a5289d6d9c7b987a6772a999084b606f568b3a94
Quan Tran <quantran@wakumo.vn>
2019-01-31 16:37:12 +0700

    update logic for get multi block txs

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -3,31 +3,31 @@
 /**
  * Module dependencies.
  */
-var Address     = require('../models/Address');
-var async       = require('async');
-var common      = require('./common');
-var util        = require('util');
+var Address = require('../models/Address');
+var async = require('async');
+var common = require('./common');
+var util = require('util');
 
-var Rpc         = require('../../lib/Rpc');
+var Rpc = require('../../lib/Rpc');
 
-var imports     = require('soop').imports();
-var bitcore     = require('bitcore');
-var RpcClient   = bitcore.RpcClient;
-var config      = require('../../config/config');
-var bitcoreRpc  = imports.bitcoreRpc || new RpcClient(config.bitcoind);
+var imports = require('soop').imports();
+var bitcore = require('bitcore');
+var RpcClient = bitcore.RpcClient;
+var config = require('../../config/config');
+var bitcoreRpc = imports.bitcoreRpc || new RpcClient(config.bitcoind);
 
 var tDb = require('../../lib/TransactionDb').default();
 var bdb = require('../../lib/BlockDb').default();
 
-exports.send = function(req, res) {
-  Rpc.sendRawTransaction(req.body.rawtx, function(err, txid) {
+exports.send = function (req, res) {
+  Rpc.sendRawTransaction(req.body.rawtx, function (err, txid) {
     if (err) {
       var message;
-      if(err.code == -25) {
+      if (err.code == -25) {
         message = util.format(
           'Generic error %s (code %s)',
           err.message, err.code);
-      } else if(err.code == -26) {
+      } else if (err.code == -26) {
         message = util.format(
           'Transaction rejected by network (code %s). Reason: %s',
           err.code, err.message);
@@ -36,31 +36,31 @@ exports.send = function(req, res) {
       }
       return res.status(400).send(message);
     }
-    res.json({'txid' : txid});
+    res.json({'txid': txid});
   });
 };
 
 exports.rawTransaction = function (req, res, next, txid) {
-    bitcoreRpc.getRawTransaction(txid, function (err, transaction) {
-        if (err || !transaction)
-            return common.handleErrors(err, res);
-        else {
-            req.rawTransaction = { 'rawtx': transaction.result };
-            return next();
-        }
-    });
+  bitcoreRpc.getRawTransaction(txid, function (err, transaction) {
+    if (err || !transaction)
+      return common.handleErrors(err, res);
+    else {
+      req.rawTransaction = {'rawtx': transaction.result};
+      return next();
+    }
+  });
 };
 
 /**
  * Find transaction by hash ...
  */
-exports.transaction = function(req, res, next, txid) {
+exports.transaction = function (req, res, next, txid) {
 
-  tDb.fromIdWithInfo(txid, function(err, tx) {
-    if (err || ! tx)
+  tDb.fromIdWithInfo(txid, function (err, tx) {
+    if (err || !tx)
       return common.handleErrors(err, res);
 
-    bdb.fillVinConfirmations(tx.info, function(err) {
+    bdb.fillVinConfirmations(tx.info, function (err) {
       if (err)
         return common.handleErrors(err, res);
 
@@ -75,7 +75,7 @@ exports.transaction = function(req, res, next, txid) {
 /**
  * Show transaction
  */
-exports.show = function(req, res) {
+exports.show = function (req, res) {
 
   if (req.transaction) {
     res.jsonp(req.transaction);
@@ -85,7 +85,7 @@ exports.show = function(req, res) {
 /**
  * Show raw transaction
  */
-exports.showRaw = function(req, res) {
+exports.showRaw = function (req, res) {
 
   if (req.rawTransaction) {
     res.jsonp(req.rawTransaction);
@@ -93,17 +93,30 @@ exports.showRaw = function(req, res) {
 };
 
 
-var getTransaction = function(txid, cb) {
-
-  tDb.fromIdWithInfo(txid, function(err, tx) {
+var getTransaction = function (txid, cb) {
+  tDb.fromIdWithInfo(txid, function (err, tx) {
     if (err) console.log(err);
 
     if (!tx || !tx.info) {
       console.log('[transactions.js.48]:: TXid %s not found in RPC. CHECK THIS.', txid);
-      return ({ txid: txid });
+      return ({txid: txid});
     }
 
     return cb(null, tx.info);
+  });
+};
+
+var blockIndex = function (bId, cb) {
+  bdb.blockIndex(bId, function (err, hashStr) {
+    if (err) console.log(err);
+    return cb(null, hashStr);
+  });
+};
+
+var fromHashWithInfo = function (hashStr, cb) {
+  bdb.fromHashWithInfo(hashStr, function (err, block) {
+    if (err) console.log(err);
+    return cb(null, block);
   });
 };
 
@@ -111,7 +124,7 @@ var getTransaction = function(txid, cb) {
 /**
  * List of transaction
  */
-exports.list = function(req, res, next) {
+exports.list = function (req, res, next) {
   var fromBlock = req.query.from;
   var toBlock = req.query.to;
 
@@ -123,46 +136,50 @@ exports.list = function(req, res, next) {
   var txLength;
   var txs;
 
-  if (fromBlock) && (toBlock) {
+  if (fromBlock && toBlock) {
 
-    for(var bIndex = fromBlock; bIndex < toBlock; bIndex ++) {
+    var bIndexes = [];
 
-      bdb.blockIndex(height, function(err, hashStr) {
-        if (err) {
-          console.log(err);
-          res.status(400).send('Bad Request');
-        } else {
-          bdb.fromHashWithInfo(hashStr, function(err, block) {
-            if (err) {
-              console.log(err);
-              return res.status(500).send('Internal Server Error');
-            }
-            txs.push(block.info.tx);
-          });
-        }
-      });
+    for (var bIndex = fromBlock; bIndex < toBlock; bIndex++) {
+      bIndexes.push(bIndex);
     }
 
-    async.mapSeries(txs, getTransaction, function(err, results) {
+    async.mapSeries(bIndexes, blockIndex, function (err, hashStrs) {
+      console.log(hashStrs);
       if (err) {
         console.log(err);
-        res.status(404).send('TX not found');
+        res.status(400).send('Bad Request');
+      } else {
+        async.mapSeries(hashStrs, fromHashWithInfo, function (err, blocks) {
+          console.log(blocks.length);
+          if (err) {
+            console.log(err);
+            return res.status(500).send('Internal Server Error');
+          }
+          for (var i = 0; i < blocks.length; i++) {
+            txs.push(blocks[i].info.tx);
+          }
+          async.mapSeries(txs, getTransaction, function (err, results) {
+            if (err) {
+              console.log(err);
+              res.status(404).send('TX not found');
+            }
+            res.jsonp({
+              txs: results
+            });
+          });
+        });
       }
-
-      res.jsonp({
-        txs: results
-      });
     });
 
-  }
-  else if (bId) {
-    bdb.fromHashWithInfo(bId, function(err, block) {
+  } else if (bId) {
+    bdb.fromHashWithInfo(bId, function (err, block) {
       if (err) {
         console.log(err);
         return res.status(500).send('Internal Server Error');
       }
 
-      if (! block) {
+      if (!block) {
         return res.status(404).send('Not found');
       }
 
@@ -172,12 +189,11 @@ exports.list = function(req, res, next) {
         var spliceInit = page * pageLength;
         txs = block.info.tx.splice(spliceInit, pageLength);
         pagesTotal = Math.ceil(txLength / pageLength);
-      }
-      else {
+      } else {
         txs = block.info.tx;
       }
 
-      async.mapSeries(txs, getTransaction, function(err, results) {
+      async.mapSeries(txs, getTransaction, function (err, results) {
         if (err) {
           console.log(err);
           res.status(404).send('TX not found');
@@ -189,11 +205,10 @@ exports.list = function(req, res, next) {
         });
       });
     });
-  }
-  else if (addrStr) {
+  } else if (addrStr) {
     var a = new Address(addrStr);
 
-    a.update(function(err) {
+    a.update(function (err) {
       if (err && !a.totalReceivedSat) {
         console.log(err);
         res.status(404).send('Invalid address');
@@ -206,12 +221,11 @@ exports.list = function(req, res, next) {
         var spliceInit = page * pageLength;
         txs = a.transactions.splice(spliceInit, pageLength);
         pagesTotal = Math.ceil(txLength / pageLength);
-      }
-      else {
+      } else {
         txs = a.transactions;
       }
 
-      async.mapSeries(txs, getTransaction, function(err, results) {
+      async.mapSeries(txs, getTransaction, function (err, results) {
         if (err) {
           console.log(err);
           res.status(404).send('TX not found');
@@ -223,8 +237,7 @@ exports.list = function(req, res, next) {
         });
       });
     });
-  }
-  else {
+  } else {
     res.jsonp({
       txs: []
     });
